62013de748d1304682d891a22ca1a6bfced77eb9
Siddharth Mishra <56888544+geeksid1800@users.noreply.github.com>
2026-04-05 19:14:39 +0530

    Add: perfect-squares - submission-1

---
+++ Data Structures & Algorithms/perfect-squares/submission-1.py
@@ -0,0 +1,26 @@
+'''
+The greedy approach isn't correct here. E.g. 18 = 16+1+1 going greedily, but
+18=9+9 is the optimal answer. This is basically the same problem as Coin Change,
+but we have to figure out which 'coins' are available to us first.
+'''
+class Solution:
+    def numSquares(self, n: int) -> int:
+        squares = []
+        for i in range(1,n+1):
+            if i*i > n: break
+            squares.append(i*i)
+        
+        print(squares)
+        dp = [None]*(n+1)
+        def recur(x):
+            if x==0: return 0
+            if x<0: return float('inf')
+            if dp[x] is not None: return dp[x]
+
+            ans = float('inf')
+            for sq in reversed(squares):
+                ans = min(ans,1+recur(x-sq))
+            dp[x] = ans
+            return ans
+        
+        return recur(n)
