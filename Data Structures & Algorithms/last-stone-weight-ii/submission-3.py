db34b97e18ad46bbed26544bcc175183f49b9127
Siddharth Mishra <56888544+geeksid1800@users.noreply.github.com>
2026-04-13 00:43:43 +0530

    Add: last-stone-weight-ii - submission-3

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-3.py
@@ -0,0 +1,36 @@
+'''
+Greedy approach doesn't work, refer notes for counterexample.
+Instead what we need to do is divide stones into 2 bags/groups, which we will try
+to make as close as possible in weight to each other. Then, we smash rocks from
+opposite bags into each other, achieving the optimal score.
+Eg. [2,7,4,1,8,1] => [7,4] and [2,1,8,1] => The difference after smashing is 1.
+Basically, we need to divide stones into group1 and group2 and minimise the
+difference between them, abs(sum(grp1)-sum(grp2)). This now becomes a simple
+problem of choosing whether to pick stone[i] in grp1 or not, which we can solve
+recursively.
+Optimization: Out of the 2^n paths/choices, there are many paths where one grp
+is much larger than the other (e.g. all stones go to grp1), which are not useful
+to our answer, as we have to minimize grp1-grp2. In fact, once grp1 crosses tot/2
+the gap between grp1 and grp2 is just going to keep increasing, so it makes sense
+to stop adding to grp1 once we reach ceil(tot/2).
+'''
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        tot = sum(stones)
+        dp = {}
+
+        def recur(i,sum1):
+            '''
+            returns the minimal difference between the two groups created so far.
+            i: ix of element we are choosing to group right now.
+            sum1: the current (cumulative) sum of group 1.
+            '''
+            if i==len(stones) or sum1>=(tot+1)//2:
+                return abs(sum1 - (tot-sum1))
+            if (i,sum1) in dp: return dp[(i,sum1)]
+
+            #we have 2 options: include or don't stones[i] in group1
+            dp[(i,sum1)] = min(recur(i+1,sum1+stones[i]), recur(i+1,sum1))
+            return dp[(i,sum1)]
+
+        return recur(0,0)
